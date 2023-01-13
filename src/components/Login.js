d1e30eb6e5d344647eb8d6e0a0af1a10bee47a29
ruona <ogheneruonaagadagba4@gmail.com>
2023-01-13 17:36:21 +0100

    set up redux, about to add more reducers

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,29 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { login, logout } from "../features/user";
 
 const Login = () => {
+  const dispatch = useDispatch();
   return (
     <div>
-      <button>Login</button>
+      <button
+        onClick={() => {
+          dispatch(
+            login({
+              name: "Ruona",
+              age: 22,
+              email: "ogheneruonaagadagba4@gmail.com",
+            })
+          );
+        }}>
+        Login
+      </button>
+      <button
+        onClick={() => {
+          dispatch(logout());
+        }}>
+        Logout
+      </button>
     </div>
   );
 };
